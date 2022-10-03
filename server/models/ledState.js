21a74adb24705b0ed702781a8bbaecb7f6054fa6
Shanre Scheepers <21100387@virtualwindow.co.za>
2022-10-03 12:43:02 +0200

    FT: Files Folders & Example Code

---
+++ server/models/ledState.js
@@ -0,0 +1,9 @@
+// Example Code
+const mongoose = require('mongoose');
+
+const ledState = mongoose.Schema({
+    name: { type: String, required: true },
+    led: { type: Boolean, required: true },
+});
+
+module.exports = mongoose.model('led', ledState);

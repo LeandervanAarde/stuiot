bbff2a73d5f0d4f0503798d689b0dbfb48adab6b
LeandervanAarde <200211@virtualwindow.co.za>
2022-10-06 11:55:59 +0200

    Homepage COmponents done
    Home page now holds the containers for the components that are required.

--- client/src/Routes/Home/Home.route.jsx
+++ client/src/Routes/Home/Home.route.jsx
@@ -7,19 +7,24 @@ import BigButton from '../../components/BigButton/BigButton.component';
 
 import fanicon from "../../assets/fanicon.svg"
 import lightsicon from '../../assets/lightsicon.svg';
+import LessUsedComponents from '../../components/LessUsedComponents/LessUsedComponents.component';
 
 const Home = () => {
     const [checked, setChecked] = useState(false)
     const [fanChecked, setFanChecked] = useState(false)
-    const toggle = (e) =>{
+    const toggle = (e) => {
         console.log(e.checked)
         setChecked(!checked)
     }
 
-    const toggleFan = (e) =>{
+    const toggleFan = (e) => {
         console.log(e.checked)
         setFanChecked(!fanChecked)
     }
+
+  const items = [1,2,3,4]
+
+
     return (
         <div className={styles.container}>
             <div className={styles.left}>
@@ -28,8 +33,11 @@ const Home = () => {
             <div className={styles.centerContainer}>
                 <GraphContainer />
                 <div className={styles.buttonContainer}>
-                    <BigButton icon={fanicon} text={"Turn on Fan"} checked={checked}  onClick={toggle}/>
+                    <BigButton icon={fanicon} text={"Turn on Fan"} checked={checked} onClick={toggle} />
                     <BigButton icon={lightsicon} text={"Turn on Lights"} checked={fanChecked} onClick={toggleFan} />
+                </div>
+                <div className={styles.otherComponents}>
+                    {items.map(i => (<LessUsedComponents/>))}
                 </div>
             </div>
             <RightNavigation />
---
+++ client/src/components/LessUsedComponents/LessUsedComponents.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from "./LessUsedComponents.module.scss";
+
+const LessUsedComponents = () => {
+    return (
+        <div className={styles.container}>
+            
+        </div>
+    );
+};
+
+export default LessUsedComponents;

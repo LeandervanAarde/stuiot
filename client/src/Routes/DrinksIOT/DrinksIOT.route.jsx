313096cffe7deb16c85f6e62af707a7d20b8b9f5
Tsebo Rams <200200@virtualwindow.co.za>
2022-10-15 03:48:40 +0200

    Added the Bubble Graph To The Route
    BubbleChart now in the drink http://localhost:3000/studrinks or in the DrinksIOT.route.jsx

--- client/src/Routes/DrinksIOT/DrinksIOT.route.jsx
+++ client/src/Routes/DrinksIOT/DrinksIOT.route.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
-
-
+import BubbleChart from '../../components/BubbleChart/BubbleChart';
+import GraphContainer from '../../components/GraphContainer/GraphContainer.component';
 
 const DrinksIOT = () => {
     return (
         <div>
-
+             <GraphContainer
+                        // id={styles.resize}
+                        children={
+                            <BubbleChart
+                                // labels={testerData.map(i => i.name)}
+                                // data={testerData.map(i => i.data)}
+                                // item={"STUdio lights"}
+                            />
+                        }
+                    />
         </div>
     );
 };

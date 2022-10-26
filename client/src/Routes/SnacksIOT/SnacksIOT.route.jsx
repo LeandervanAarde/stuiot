01d5d2e9e6796233d4ed35ad1ec6c60171dd27b6
Shanre Scheepers <21100387@virtualwindow.co.za>
2022-10-26 21:11:08 +0200

    STUsnacks DONE

--- client/src/Routes/SnacksIOT/SnacksIOT.route.jsx
+++ client/src/Routes/SnacksIOT/SnacksIOT.route.jsx
@@ -1,28 +1,56 @@
 import React from 'react';
-import { Button } from '@mui/material';
+import { Button, linearProgressClasses } from '@mui/material';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import axios from 'axios';
 import ReportProblemOutlinedIcon from '@mui/icons-material/ReportProblemOutlined';
 import '../../main scss/mainPage.scss';
 const SnacksIOT = () => {
-    const [dispensingState, setDispensingState] = useState(false)
+    const [dispensingState, setDispensingState] = useState(true)
     useEffect(() => {
         const interval = setInterval(() => {
             axios.get('http://localhost/api/getDispenserState/')
                 .then((res) => {
-
+                    console.log(res.data);
+                    setDispensingState(res.data.dispensing)
                 })
+                .catch(function (error) {
+                    console.log(error);
+                })
+        }, 5000);
+
+    }, [])
+
+    const alertActive = () => {
+        setDispensingState(true);
+
+        let payloadData = {
+            dispensing: true,
+        }
+
+        console.log("loggin");
+        axios.patch('http://localhost/api/updateDispenserState/', payloadData).then((res) => {
+            if (res) {
+                console.log("Dispensed clicked");
+            }
         })
-    })
-
-
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
     return (
         <>
-            <Button variant="contained" style={{ backgroundColor: '#23257A' }}>Dispense</Button>
+            <Button variant="contained" style={{ backgroundColor: '#23257A' }} onClick={() => alertActive()}>Dispense</Button>
             <div className='dispensing'>
-                <p style={{ color: '#ffff' }}>Dispensing: </p>
-                <ReportProblemOutlinedIcon />
+                {!dispensingState ? (
+                    <div className='dispensing__alert'>
+                        < ReportProblemOutlinedIcon />
+                    </div>
+                ) : (
+                    <div className='dispensing__alertactive'>
+                        < ReportProblemOutlinedIcon />
+                    </div>
+                )}
             </div>
         </>
     );
